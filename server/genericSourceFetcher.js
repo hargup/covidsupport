78c2e1e22c5011400cb89333c95c0ffec17cbd1b
Dennish Jose <dennish.jose@redbus.com>
2021-04-28 20:39:04 +0530

    plasma resources added; error checks; state field

--- server/genericSourceFetcher.js
+++ server/genericSourceFetcher.js
@@ -1,159 +1,109 @@
 const axios = require('axios')
-var moment = require('moment')
+const{genericCovidHospitals, covidresourcesInDataProcessor} =require("./source_helpers/generalPostProcessor.js")
 const _ = require('lodash-contrib');
 
-function generalPostProcessor(item) {
-    const sample = {
-        "hospital_category": "DCHC",
-        "total_beds_allocated_to_covid": 16,
-        "total_beds_without_oxygen": 16,
-        "available_beds_without_oxygen": 16,
-        "total_beds_with_oxygen": 0,
-        "available_beds_with_oxygen": 0,
-        "total_icu_beds_without_ventilator": 0,
-        "available_icu_beds_without_ventilator": 0,
-        "total_icu_beds_with_ventilator": 0,
-        "available_icu_beds_with_ventilator": 0,
-        "hospital_name": "24 * 7  Covid Care Center",
-        "area": "East",
-        "hospital_address": "24* 7 Covid care Center  behind Canta Care Center  bhabha Nagar Nashik",
-        "hospital_phone": "8975042579",
-        "officer_name": "",
-        "officer_designation": "",
-        "last_updated_on": 1619274352597
-    }
-    // TODO: Add a total number of beds in key bedCount
-    const renameMap = {"hospital_name": "hospital",
-                       "available_beds_without_oxygen": "bedCount",
-                       "available_beds_with_oxygen": "oxygenBeds",
-                       "available_icu_beds_without_ventilator": "icuCount",
-                       "available_icu_beds_with_ventilator": "ventilatorCount",
-                       "hospital_address": "address",
-                       "hospital_phone": "contactNumber",
-                       "last_updated_on": "verifiedAt",
-                       "district": "city"}
-    const newItem =  _.renameKeys(item, renameMap)
-    const tz = moment.unix(newItem.verifiedAt/1000).toISOString()
-    return {...newItem, verifiedAt: tz}
-    
-}
 
-function covidresourcesInDataProcessor(city,cityData){
-    const renameMap = {"name": "hospital",
-                        "description": "others",
-                       "name": "address",
-                       "contact": "contactPerson",
-                       "contact": "contactNumber",
-                       "lastVerified": "verifiedAt",
-                       "district": "city"
-                        }
-    if(!cityData.length){
-        return [];
-    }
-    else{
 
-        cityData.map(resource => {
-            
-            resource.resources=[];
-            if(resource.category.indexOf("Bed")>=0)
-                resource.resources.push("beds");
-            if(resource.category.indexOf("Oxygen")>=0)
-                resource.resources.push("oxygen")
-            if(resource.category.indexOf("Plasma")>=0)
-                resource.resources.push("plasma")
-            resource.city = city;
-            // resource = _.renameKeys(resource, renameMap)
-            // TODO: rename keys to match FE
-            
-        })
-        //cityData.city= city;
-        return cityData;
-    }
-}
-
-async function nashikCovidHospitals() {
-    const {data} = await axios.get("https://covidnashik.com/data/covidnashik.com/bed_data.json")
-    return data.map(generalPostProcessor).map(item=>{return {...item, city: "nashik", resources: ["beds"]}})
+async function nashikCovidHospitals(){
+    const bedsUrl = "https://covidnashik.com/data/covidnashik.com/bed_data.json";
+    const plasmaUrl = "https://covidnashik.com/data/covidnashik.com/plasma_data.json"
+    return await genericCovidHospitals(bedsUrl,plasmaUrl,'nashik','Maharashtra');
 }
 
 async function puneCovidHospitals() {
-    const {data} = await axios.get("https://covidpune.com/data/covidpune.com/bed_data.json")
-    return data.map(generalPostProcessor).map(item=>{return {...item, city: "pune", resources: ["beds"]}})
+    const bedsUrl = "https://covidpune.com/data/covidpune.com/bed_data.json";
+    const plasmaUrl = "https://covidpune.com/data/covidpune.com/plasma_data.json"
+    return await genericCovidHospitals(bedsUrl,plasmaUrl,'pune','Maharashtra');
 }
 
 async function gandhinagarCovidHospitals() {
-    const {data} = await axios.get("https://covidgandhinagar.com/data/covidgandhinagar.com/bed_data.json")
-    return data.map(generalPostProcessor).map(item=>{return {...item, city: "gandhinagar", resources: ["beds"]}})
+    const bedsUrl = "https://covidgandhinagar.com/data/covidgandhinagar.com/bed_data.json";
+    const plasmaUrl = "https://covidgandhinagar.com/data/covidgandhinagar.com/plasma_data.json"
+    return await genericCovidHospitals(bedsUrl,plasmaUrl,'gandhinagar','Gujrat');
 }
 
 async function amdCovidHospitals() {
-    const {data} = await axios.get("https://covidamd.com/data/covidamd.com/bed_data.json")
-    return data.map(generalPostProcessor).map(item=>{return {...item, city: "ahmedabad", resources: ["beds"]}})
+    const bedsUrl = "https://covidamd.com/data/covidamd.com/bed_data.json";
+    const plasmaUrl = "https://covidamd.com/data/covidamd.com/plasma_data.json"
+    return await genericCovidHospitals(bedsUrl,plasmaUrl,'ahmedabad','Gujrat');
 }
 
 async function beedCovidHospitals() {
-    const {data} = await axios.get("https://covidbeed.com/data/covidbeed.com/bed_data.json")
-    return data.map(generalPostProcessor).map(item=>{return {...item, city: "beed", resources: ["beds"]}})
+    const bedsUrl = "https://covidbeed.com/data/covidbeed.com/bed_data.json";
+    const plasmaUrl = "https://covidbeed.com/data/covidbeed.com/plasma_data.json"
+    return await genericCovidHospitals(bedsUrl,plasmaUrl,'beed','Maharashtra');
 }
 
 async function tnaduCovidHospitals() {
     // NOTE: This is different from other covid<region>.com website, this is for multiple cities
     // inside Tamil Nadu
-    const {data} = await axios.get("https://covidtnadu.com/data/covidtnadu.com/bed_data.json")
-    return data.map(generalPostProcessor).map(item=>{return {...item, resources: ["beds"]}})
+    const bedsUrl = "https://covidtnadu.com/data/covidtnadu.com/bed_data.json";
+    const plasmaUrl = "https://covidtnadu.com/data/covidtnadu.com/plasma_data.json"
+    return await genericCovidHospitals(bedsUrl,plasmaUrl,'','Tamil Nadu');
 }
 
 async function AndhraCovidHospitals() {
-    const {data} = await axios.get("https://covidaps.com/data/covidaps.com/bed_data.json")
-    return data.map(generalPostProcessor).map(item=>{return {...item, resources: ["beds"]}})
+    const bedsUrl = "https://covidaps.com/data/covidaps.com/bed_data.json";
+    const plasmaUrl = "https://covidaps.com/data/covidaps.com/plasma_data.json"
+    return await genericCovidHospitals(bedsUrl,plasmaUrl,'','Andhra Pradesh');
 }
 
 async function delhiCovidHospitals(){
-    const {data} = await axios.get("https://coviddelhi.com/data/coviddelhi.com/bed_data.json")
-    return data.map(generalPostProcessor).map(item=>{return {...item, resources: ["beds"]}})
+    const bedsUrl = "https://coviddelhi.com/data/coviddelhi.com/bed_data.json";
+    const plasmaUrl = "https://coviddelhi.com/data/coviddelhi.com/plasma_data.json"
+    return await genericCovidHospitals(bedsUrl,plasmaUrl,'delhi','Delhi');
 }
 async function barodaCovidHospitals(){
-    const {data} = await axios.get("https://covidbaroda.com/data/covidbaroda.com/bed_data.json")
-    return data.map(generalPostProcessor).map(item=>{return {...item, resources: ["beds"]}})
+    const bedsUrl = "https://covidbaroda.com/data/covidbaroda.com/bed_data.json";
+    const plasmaUrl = "https://covidbaroda.com/data/covidbaroda.com/plasma_data.json"
+    return await genericCovidHospitals(bedsUrl,plasmaUrl,'baroda','Gujrat');
 }
 
 async function bengaluruCovidHospitals(){
-    const {data} = await axios.get("https://covidbengaluru.com/data/covidbengaluru.com/bed_data.json")
-    return data.map(generalPostProcessor).map(item=>{return {...item, resources: ["beds"]}})
+    const bedsUrl = "https://covidbengaluru.com/data/covidbengaluru.com/bed_data.json";
+    const plasmaUrl = "https://covidbengaluru.com/data/covidbengaluru.com/plasma_data.json"
+    return await genericCovidHospitals(bedsUrl,plasmaUrl,'bengaluru','Karnataka');
 }
 async function chattisgarhCovidHospitals(){
-    const {data} = await axios.get("https://covidcgh.com/data/covidcgh.com/bed_data.json")
-    return data.map(generalPostProcessor).map(item=>{return {...item, resources: ["beds"]}})
+    const bedsUrl = "https://covidcgh.com/data/covidcgh.com/bed_data.json";
+    const plasmaUrl = "https://covidcgh.com/data/covidcgh.com/plasma_data.json"
+    return await genericCovidHospitals(bedsUrl,plasmaUrl,'','Chhattisgarh');
 }
 
 async function madhyaPradeshCovidHospitals(){
-    const {data} = await axios.get("https://covidmp.com/data/covidmp.com/bed_data.json")
-    return data.map(generalPostProcessor).map(item=>{return {...item, resources: ["beds"]}})
+    const bedsUrl = "https://covidmp.com/data/covidmp.com/bed_data.json";
+    const plasmaUrl = "https://covidmp.com/data/covidmp.com/plasma_data.json"
+    return await genericCovidHospitals(bedsUrl,plasmaUrl,'','Madhya Pradesh');
 }
 
 async function nagpurCovidHospitals(){
-    const {data} = await axios.get("https://covidnagpur.com/data/covidnagpur.com/bed_data.json")
-    return data.map(generalPostProcessor).map(item=>{return {...item, resources: ["beds"]}})
+    const bedsUrl = "https://covidnagpur.com/data/covidnagpur.com/bed_data.json";
+    const plasmaUrl = "https://covidnagpur.com/data/covidnagpur.com/plasma_data.json"
+    return await genericCovidHospitals(bedsUrl,plasmaUrl,'nagpur','Maharashtra');
 }
 
 async function rajkotCovidHospitals(){
-    const {data} = await axios.get("https://covidrkt.com/data/covidrkt.com/bed_data.json")
-    return data.map(generalPostProcessor).map(item=>{return {...item, resources: ["beds"]}})
+    const bedsUrl = "https://covidrkt.com/data/covidrkt.com/bed_data.json";
+    const plasmaUrl = "https://covidrkt.com/data/covidrkt.com/plasma_data.json"
+    return await genericCovidHospitals(bedsUrl,plasmaUrl,'rajkot','Gujrat');
 }
 
 async function suratCovidHospitals(){
-    const {data} = await axios.get("https://covidsurat.com/data/covidsurat.com/bed_data.json")
-    return data.map(generalPostProcessor).map(item=>{return {...item, resources: ["beds"]}})
+    const bedsUrl = "https://covidsurat.com/data/covidsurat.com/bed_data.json";
+    const plasmaUrl = "https://covidsurat.com/data/covidsurat.com/plasma_data.json"
+    return await genericCovidHospitals(bedsUrl,plasmaUrl,'surat','Gujrat');
 }
 
 async function bengalCovidHospitals(){
-    const {data} = await axios.get("https://covidwb.com/data/covidwb.com/bed_data.json")
-    return data.map(generalPostProcessor).map(item=>{return {...item, resources: ["beds"]}})
+    const bedsUrl = "https://covidwb.com/data/covidwb.com/bed_data.json";
+    const plasmaUrl = "https://covidwb.com/data/covidwb.com/plasma_data.json"
+    return await genericCovidHospitals(bedsUrl,plasmaUrl,'','West Bengal');
 }
 
 async function telanganaCovidHospitals(){
-    const {data} = await axios.get("https://covidtelangana.com/data/covidtelangana.com/bed_data.json")
-    return data.map(generalPostProcessor).map(item=>{return {...item, resources: ["beds"]}})
+    const bedsUrl = "https://covidtelangana.com/data/covidtelangana.com/bed_data.json";
+    const plasmaUrl = "https://covidtelangana.com/data/covidtelangana.com/plasma_data.json"
+    return await genericCovidHospitals(bedsUrl,plasmaUrl,'','Telangana');
 }
 
 async function covidresourcesIn(){
